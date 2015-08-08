0e9ec139d343178d481dba5bb7708b1c83dcf3bd
mikejonesey <michael.jones@linux.com>
2015-08-08 01:12:26 +0100

    adding page faults, runtime and core

--- take-stacks.sh
+++ take-stacks.sh
@@ -8,17 +8,22 @@
 ################################################
 #
 
-TRACE_COUNT=1
+TRACE_COUNT=10
+TRACE_INTERVAL=1
 JAVA_PID=$(ps -ef | grep java | grep -v grep | tail -1 | awk '{print $2}')
+TMP_DIR="/tmp/$(date +"%Y-%m-%d")"
+
+mkdir $TMP_DIR 2>/dev/null
 
 for ((i=0; i<$TRACE_COUNT; i++)); do
   TRACETIME=$(date +"%s")
-  ps -eLo pcpu,pid,lwp,cmd | grep java | grep -v grep | awk '{print $1 " " $3}' | sort -k1,1 -n | while read al; do nid=$(printf "0x%x\n" $(echo "$al" | awk '{print $2}')); echo "$al $nid" | awk '{print $1 " " $3}'; done > /tmp/nid-rep-$TRACETIME.out
-  jstack -l "$JAVA_PID" > /tmp/stack-$TRACETIME.out
-  cat /tmp/nid-rep-$TRACETIME.out | while read al; do
-    PCPU=$(echo "$al" | awk '{print $1}')
-    NID=$(echo "$al" | awk '{print $2}')
-    sed -i "s/nid=$NID/nid=$NID pcpu=$PCPU/" /tmp/stack-$TRACETIME.out
+  EXINF=$(ps -eLo pcpu,lwp,sgi_p,maj_flt,min_flt,etimes,cmd | grep java | grep -v grep | sort -k1,1 -n | while read al; do nid=$(printf "0x%x\n" $(echo "$al" | awk '{print $2}')); echo "$nid $al"; done)
+  jstack -l "$JAVA_PID" > $TMP_DIR/stack-$TRACETIME.out
+  echo "$EXINF" | while read al; do
+    NID=$(echo "$al" | awk '{print $1}')
+    ESTR="$(echo "$al" | awk '{print "pcpu=" $2 " core=" $4 " page=" $5 " mpage=" $6 " secs=" $7}')"
+    sed -i "s/nid=$NID/nid=$NID $ESTR/" $TMP_DIR/stack-$TRACETIME.out
   done
+  sleep $TRACE_INTERVAL
 done
 
