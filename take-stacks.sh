ee269599e59ec39a5c3a3e50c465d32eede55487
mikejonesey <michael.jones@linux.com>
2015-10-11 22:50:19 +0100

    backwards compatibility with ps, etimes
    (older ps don't have etimes)

--- take-stacks.sh
+++ take-stacks.sh
@@ -15,15 +15,30 @@ TMP_DIR="/tmp/$(date +"%Y-%m-%d")"
 
 mkdir $TMP_DIR 2>/dev/null
 
+ps -o etimes &>/dev/null
+if [ "$?" == "0" ]; then
+	etimes_valid=true
+else
+	etimes_valid=false
+fi
+
 for ((i=0; i<$TRACE_COUNT; i++)); do
-  TRACETIME=$(date +"%s")
-  EXINF=$(ps -eLo pcpu,lwp,sgi_p,maj_flt,min_flt,etimes,cmd | grep java | grep -v grep | sort -k1,1 -n | while read al; do nid=$(printf "0x%x\n" $(echo "$al" | awk '{print $2}')); echo "$nid $al"; done)
-  jstack -l "$JAVA_PID" > $TMP_DIR/stack-$TRACETIME.out
-  echo "$EXINF" | while read al; do
-    NID=$(echo "$al" | awk '{print $1}')
-    ESTR="$(echo "$al" | awk '{print "pcpu=" $2 " core=" $4 " page=" $5 " mpage=" $6 " secs=" $7}')"
-    sed -i "s/nid=$NID/nid=$NID $ESTR/" $TMP_DIR/stack-$TRACETIME.out
-  done
-  sleep $TRACE_INTERVAL
+	TRACETIME=$(date +"%s")
+	if [ $etimes_valid == true ]; then
+		EXINF=$(ps -eLo pcpu,lwp,sgi_p,maj_flt,min_flt,etimes,cmd | grep java | grep -v grep | sort -k1,1 -n | while read al; do nid=$(printf "0x%x\n" $(echo "$al" | awk '{print $2}')); echo "$nid $al"; done)
+	else
+		EXINF=$(ps -eLo pcpu,lwp,sgi_p,maj_flt,min_flt,cmd | grep java | grep -v grep | sort -k1,1 -n | while read al; do nid=$(printf "0x%x\n" $(echo "$al" | awk '{print $2}')); echo "$nid $al"; done)
+	fi
+	jstack -l "$JAVA_PID" > $TMP_DIR/stack-$TRACETIME.out
+	echo "$EXINF" | while read al; do
+		NID=$(echo "$al" | awk '{print $1}')
+		if [ $etimes_valid == true ]; then
+			ESTR="$(echo "$al" | awk '{print "pcpu=" $2 " core=" $4 " page=" $5 " mpage=" $6 " secs=" $7}')"
+		else
+			ESTR="$(echo "$al" | awk '{print "pcpu=" $2 " core=" $4 " page=" $5 " mpage=" $6}')"
+		fi
+		sed -i "s/nid=$NID/nid=$NID $ESTR/" $TMP_DIR/stack-$TRACETIME.out
+	done
+	sleep $TRACE_INTERVAL
 done
 
